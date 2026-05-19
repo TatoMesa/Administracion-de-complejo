1cb663f3cf96fba981c6a5381d007b00f0ac6c45
tatomesa <seba_mesa@hotmail.com>
2026-05-19 21:15:53 +0000

    feat: configuracion de settings base/development/production

---
+++ config/settings/production.py
@@ -0,0 +1,17 @@
+from .base import *
+import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
